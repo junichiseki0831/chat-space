64bded430670b69c9aece033632eb832e7a445dc
関純一 <>
2018-05-05 13:47:42 +0900

    メッセージ送信機能の非同期通信化

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
       g.javascripts false
       g.helper false
@@ -16,5 +18,6 @@ module ChatSpace
     end
 
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
   end
 end
