68791558b818626f76e5874199a154dc044b8d55
関純一 <>
2018-05-08 21:13:43 +0900

    HTMLビュー作成

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,14 @@
 $(function(){
-  $("#user-search-field").on('keyup', function(){
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    users.append(html)
+  }
+
+  $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
 
     $.ajax({
@@ -10,6 +19,13 @@ $(function(){
     })
 
     .done(function(users) {
-    });
+      users.forEach(function(user){
+        appendUser(user)
+      });
+    })
+
+    .fail(function() {
+      alert('ユーザーの検索に失敗しました')
+    })
   });
-})
+});
